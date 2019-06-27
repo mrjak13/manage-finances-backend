d7922ade270c321c4c0b61537979955f2c15ebec
Justin Kenney <justinkenney13@gmail.com>
2019-06-27 13:01:09 -0400

    add sessions controller/fix auth token bug

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,2 +1,9 @@
-class SessionsController < ApplicationController
+class Api::V1::SessionsController < ApplicationController
+	protect_from_forgery with: :null_session
+
+	def create
+		binding.pry
+		
+	end
+
 end
