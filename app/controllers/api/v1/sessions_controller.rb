af430975bf578742930d78330ebb10500fa9ce30
Justin Kenney <justinkenney13@gmail.com>
2019-06-27 14:40:00 -0400

    add ability to create session

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -2,8 +2,21 @@ class Api::V1::SessionsController < ApplicationController
 	protect_from_forgery with: :null_session
 
 	def create
-		binding.pry
-		
+		@user = User.find_by(email: params[:session][:email])
+
+		if @user && @user.authenticate(params[:session][:password])
+			session[:user_id] = @user.id
+			render json: @user
+		else
+			render json: {
+				error: "Invalid Credentials"
+			}
+	end
+
+	private
+
+	def sessionParams
+		params.require(:session).permit(:email, :password)
 	end
 
 end
