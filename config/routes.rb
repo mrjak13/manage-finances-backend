5777863405ff608e00bc5fae8c126c33adb9dfa5
Justin Kenney <justinkenney13@gmail.com>
2019-06-28 17:04:32 -0400

    set up nested routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,10 +7,11 @@ Rails.application.routes.draw do
 
 	namespace :api do
 		namespace :v1 do
-
-		  resources :transactions
-		  resources :accounts
-		  resources :users
+		  resources :users do
+			  resources :accounts do
+				  resources :transactions
+			  end
+		  end
 		end
 	end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
