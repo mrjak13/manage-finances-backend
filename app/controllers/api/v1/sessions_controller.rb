fd53d2312645a5bf8bdb9cb1a9b9dc5a7b4c79a3
Justin Kenney <justinkenney13@gmail.com>
2019-06-29 12:07:27 -0400

    now renders serialized data/reconfigured nested routes

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,12 +1,15 @@
 class Api::V1::SessionsController < ApplicationController
 	# protect_from_forgery with: :null_session
 
+
 	def create
 		@user = User.find_by(email: params[:session][:email])
 
 		if @user && @user.authenticate(params[:session][:password])
 			session[:user_id] = @user.id
-			render json: @user
+
+			user_json = UserSerializer.new(@user).serialized_json
+			render json: user_json
 		else
 			render json: {
 				error: "Invalid Credentials"
@@ -16,7 +19,8 @@ class Api::V1::SessionsController < ApplicationController
 
 	def get_current_user
 		if logged_in?
-			render json: current_user
+			user_json = UserSerializer.new(current_user).serialized_json
+			render json: user_json
 		else
 			render json: {
 				error: "No one logged in"
--- config/routes.rb
+++ config/routes.rb
@@ -7,11 +7,15 @@ Rails.application.routes.draw do
 
 	namespace :api do
 		namespace :v1 do
-		  resources :users do
-			  resources :accounts do
-				  resources :transactions
-			  end
+
+		  resources :users do 
+		  	resources :accouts
 		  end
+
+			resources :accounts do
+				resources :transactions
+		  end
+
 		end
 	end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
