0444f80dfb25c02afe4e540a840efc0b2336d450
Justin Kenney <justinkenney13@gmail.com>
2019-07-06 15:23:37 -0400

    change dafault balance to 0

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_06_162130) do
+ActiveRecord::Schema.define(version: 2019_07_06_183108) do
 
   create_table "accounts", force: :cascade do |t|
     t.string "name"
-    t.float "balance"
+    t.float "balance", default: 0.0
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
